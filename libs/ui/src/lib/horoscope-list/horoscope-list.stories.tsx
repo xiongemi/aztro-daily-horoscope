6df2765579f2e953d8ef598cabdce898f93aaa0f
Emily Xiong <xiongemi@gmail.com>
2021-06-02 19:41:06 -0400

    add storybook

---
+++ libs/ui/src/lib/horoscope-list/horoscope-list.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HoroscopeList } from './horoscope-list';
+
+export default {
+  component: HoroscopeList,
+  title: 'HoroscopeList',
+};
+
+export const primary = () => {
+  return <HoroscopeList />;
+};

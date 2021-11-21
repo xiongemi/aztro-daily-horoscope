1a6b6453d3f3e44eb2e6cedf01e4148b720eff02
Emily Xiong <xiongemi@gmail.com>
2021-11-20 23:37:21 -0500

    upgrade nx to 13.2.1

--- apps/daily-horoscope-app/jest.config.js
+++ apps/daily-horoscope-app/jest.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  displayName: 'daily-horoscope',
+  displayName: 'daily-horoscope-app',
   preset: 'react-native',
+  testRunner: 'jest-jasmine2',
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|react-native-elements|react-native-size-matters|react-native-vector-icons|@react-navigation)',
   ],
+  moduleNameMapper: {
+    '.svg': '@nrwl/react-native/plugins/jest/svg-mock',
+  },
   transform: {
     '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
     '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(
       'react-native/jest/assetFileTransformer.js'
     ),
-  },
-  moduleNameMapper: {
-    '.svg': require.resolve('@nrwl/react-native/svg-mock.js'),
   },
 };
--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  displayName: 'daily-horoscope',
+  displayName: 'ui',
   preset: 'react-native',
+  testRunner: 'jest-jasmine2',
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
-  "transformIgnorePatterns": [
-    "node_modules/(?!(@react-native|react-native|react-native-elements|react-native-size-matters|react-native-vector-icons|@react-navigation)/)"
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-elements|react-native-size-matters|react-native-vector-icons|@react-navigation)/)',
   ],
+  moduleNameMapper: {
+    '.svg': '@nrwl/react-native/plugins/jest/svg-mock',
+  },
   transform: {
     '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
     '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(

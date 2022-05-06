b7e670fc7353e03d4281d163518550cc3f7c60b9
Emily Xiong <xiongemi@gmail.com>
2022-05-06 17:29:33 -0400

    upgrade to nx 14.0.5

--- apps/daily-horoscope-app/src/main.tsx
+++ apps/daily-horoscope-app/src/main.tsx
@@ -6,4 +6,4 @@ import App from './app/App';
 
 NativeTachyons.build({}, StyleSheet);
 
-AppRegistry.registerComponent('main', () => App);
+AppRegistry.registerComponent('DailyHoroscopeApp', () => App);
--- libs/services/jest.config.ts
+++ libs/services/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'models',
-  preset: '../../jest.preset.js',
+
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,4 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/models',
+  preset: '../../jest.preset.ts',
 };
